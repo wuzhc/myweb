b38b9b0d9b712d3425909c9108a61505bd786872
wuzhc <1716220125@qq.com>
2016-11-24 16:33:23 +0800

    add article edit in the front

--- frontend/views/article/view.php
+++ frontend/views/article/view.php
@@ -19,7 +19,7 @@ $this->params['breadcrumbs'][] = $content->title;
             <?php echo Html::encode($content->title) ?>
         </p>
         <p><span class="glyphicon glyphicon-eye-open"></span>：<?php echo $content->hits;?>&nbsp;&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-calendar"></span>：<?php echo date('Y-m-d', $content->create_at)?>
-            &nbsp;&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-cog"></span>：<a href="./admin.php?r=article/update&id=<?php echo $content->id?>">
+            &nbsp;&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-cog"></span>：<a href="./admin.php?r=article/update&id=<?php echo $content->id?>">edit</a>
         </p>
         <br />
         <p>
