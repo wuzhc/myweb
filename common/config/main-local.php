f9f246fd45d0d884e451ff632d113c02542fab97
wuzhc <wzc1716220125@163.com>
2016-12-25 11:50:04 +0800

    add upload image for wangeditor

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=myweb',
             'username' => 'root',
-            'password' => '',
+            'password' => 'wuzhc2580',
             'charset' => 'utf8',
         ],
         'mailer' => [
