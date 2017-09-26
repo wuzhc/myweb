a8f0b4eddcf5f6d8fb70b37221ab8cea3a54b6db
wuzhc <1716220125@qq.com>
2017-09-26 17:16:29 +0800

    pretty url

--- frontend/views/article/_article_list.php
+++ frontend/views/article/_article_list.php
@@ -7,12 +7,13 @@ use yii\helpers\StringHelper;
 <div class="media-body">
     <h4 class="media-heading" style="font-weight: bold">
         <span class="glyphicon glyphicon-tags" style="margin-right: 10px;color: #337ab7"></span>
-        <?php echo Html::a(StringHelper::truncate(Html::encode($model->title), 35),['article/view','id'=>$model->id,'parentID'=>$_GET['parentID']],['style'=>array('color'=>'#000000')])?>
+        <a href="<?=\yii\helpers\Url::to(['article/view','id'=>$model->id,'parentID'=>$_GET['parentID']])?>" style="color:#000000">
+            <?=StringHelper::truncate(Html::encode($model->title), 35)?>
+        </a>
     </h4>
     <p style="font-size: 12px;color: #777">
         <span class="glyphicon glyphicon-eye-open"></span>：<?php echo $model->hits;?>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<!--        <span class="glyphicon glyphicon-calendar"></span>：--><?php //echo date('Y-m-d', $model->create_at); ?>
     </p>
     <p style='font-size: 14px;line-height:24px;'><?php echo HtmlPurifier::process($model->summary) ?></p>
 </div>
