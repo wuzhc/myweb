6a83c2bfba9491fbae947ef8e2b92613a1bed3e1
wuzhc <1716220125@qq.com>
2017-10-11 17:56:25 +0800

    comment

---
+++ console/migrations/m171011_095533_comment.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m171011_095533_comment extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m171011_095533_comment cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
